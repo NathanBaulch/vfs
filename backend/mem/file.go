0293e9c8722e295b32b613aad501aa5cc72d6561
Nathan Baulch <nathan.baulch@gmail.com>
2025-01-09 10:21:00 +1100

    golangci-lint wastedassign

--- backend/mem/file.go
+++ backend/mem/file.go
@@ -443,9 +443,7 @@ func (f *File) Delete(...options.DeleteOption) error {
 			// casting a file to the object's "i" interface
 			file := thisObj.i.(*memFile)
 			file.exists = false
-			file = nil
 			thisObj.i = nil
-			thisObj = nil
 			// setting that key to nil so it truly no longer lives on this system
 			delete(mapRef[loc.Volume()], str)
 		}
